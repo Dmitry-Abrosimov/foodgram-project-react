93c2a5ee36cfe7bea4d559b6e93aff63cd22b11f
Дмитрий Абросимов <dmitry.713@yandex.ru>
2022-08-29 17:30:02 +0700

    users

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,59 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    """Кастомная модель пользователя."""
+
+    username = models.CharField(
+        max_length=150,
+        unique=True,        
+        verbose_name='Имя пользователя',
+    )
+    password = models.CharField(
+        max_length=150,
+        verbose_name='Пароль',
+    )
+    email = models.EmailField(
+        max_length=254,
+        unique=True,
+        verbose_name='Email',
+    )
+    first_name = models.CharField(
+        max_length=150,
+        verbose_name='Имя',
+    )
+    last_name = models.CharField(
+        max_length=150,
+        verbose_name='Фамилия',
+    )
+
+    class Meta(AbstractUser.Meta):
+        ordering = ('username',)
+
+
+class Follow(models.Model):
+    """Модель подписок."""
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='follower',
+        verbose_name='Подписчик',
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='following',
+        verbose_name='Автор',
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                name='unique_follows',
+                fields=['user', 'author'],
+            ),
+        ]
+        verbose_name = 'Подписчик'
+        verbose_name_plural = 'Подписчики'
