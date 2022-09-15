49ddadb5579156360d61106c910c94c59e7d4d83
Дмитрий Абросимов <dmitry.713@yandex.ru>
2022-09-15 20:00:04 +0700

    deploy_16

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = '85gw#f8u^+vgxwomblqaltk8ukl+!5v04q6z56t)@^%43ag00f'
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '*',
